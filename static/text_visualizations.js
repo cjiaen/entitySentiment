8540f6c404c59a8cc8e8c9bdf673c9e8b60fa645
Tan Jia En Calvin  (NCS) <calvintan@ncs.com.sg>
2018-12-06 00:08:37 +0800

    updated visual

--- static/text_visualizations.js
+++ static/text_visualizations.js
@@ -1,7 +1,196 @@
-$.getJSON('/static/test_output.json', function(data){
+//load in json data file
+/*
+d3.json('/static/test_output.json', function(data){
     console.log(data);
 });
+*/
+$.getJSON('/static/test_output.json', function(data){
+    var nodes_dict = {};
+    var links = [];
+    var width = 960, height = 500;
+
+    // Compute the distinct nodes from the links.
+    data['relationship'].forEach(function(link) {
+        //link.subject = nodes[link.subject] || (nodes[link.subject] = {name: link.subject});
+        //link.object = nodes[link.object] || (nodes[link.object] = {name: link.object});
+        nodes_dict[link.subject] = {name: link.subject};
+        nodes_dict[link.object] = {name: link.object};
+        var score = link.sentiment;
+        var type = "neutral";
+        
+        if(score < 0){
+            type = 'nagative'
+        } else {
+            type = 'positive'
+        };
+        
+        links.push({'source':link.subject, 'target':link.object, 'score':score, 'type':type});
+    });
+
+    var nodes = Object.values(nodes_dict);
+
+    console.log(nodes);
+    console.log(links);
+
+    //set up the simulation 
+    var simulation = d3.forceSimulation()
+        .nodes(nodes);
+
+    var link_force =  d3.forceLink(links)
+                        .id(function(d) { return d.name; });
+    simulation
+        .force("charge_force", d3.forceManyBody())
+        .force("center_force", d3.forceCenter(width / 2, height / 2))
+        .force("links",link_force);
+
+    //add tick instructions: 
+    simulation.on("tick", tickActions);
+
+    //set up svg canvas
+    var svg = d3.select("body").append("svg")
+        .attr("width", width)
+        .attr("height", height);
+   
+    var arcs = svg.append('g')
+                    .selectAll('path')
+                    .data(links)
+                    .enter()
+                    .append("path")
+                    .attr("class", "links")
+                    .attr("marker-end", function(d) { return "url(#" + d.index + ")"; });
+    
+    var circle = svg.append('g')
+                    .attr("class", "nodes")
+                    .selectAll('circle')
+                    .data(nodes)
+                    .enter()
+                    .append('circle')
+                    .attr('r', 6)
+                    .attr("fill", "grey");
+
+    svg.append("defs")
+        .selectAll("marker")
+        .data(links)
+        .enter()
+        .append("marker")
+        .attr("id", function(d) { return d.index; })
+        .attr("viewBox", "0 -5 10 10")
+        .attr("refX", 15)
+        .attr("refY", -1.5)
+        .attr("markerWidth", 6)
+        .attr("markerHeight", 6)
+        .attr("orient", "auto")
+        .append("path")
+        .attr("d", "M0,-5L10,0L0,5");
+
+    var text = svg.append('g')
+                    .attr('class', "text_label")
+                    .selectAll('text')
+                    .data(nodes)
+                    .enter()
+                    .append('text')
+                    .attr('x', 10)
+                    .attr('y', 5)
+                    .text(function(d){return d.name;});
+    
+    var drag_handler = d3.drag()
+                        .on("drag", drag_drag);	
+
+    drag_handler(nodes);
+
+    function drag_drag(d) {
+        d.x = d3.event.x;
+        d.y = d3.event.y;
+    }
+
+    function linkArc(d) {
+        var dx = d.target.x - d.source.x,
+            dy = d.target.y - d.source.y,
+            dr = Math.sqrt(dx * dx + dy * dy);
+        return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
+    } 
+
+    function tickActions() {
+        //update circle positions each tick of the simulation 
+        circle
+            .attr('transform', transform);
+            
+        arcs
+            .attr('d', linkArc);
+        
+        text
+            .attr('transform', transform);
+    }
+
+    function transform(d) {
+        return "translate(" + d.x + "," + d.y + ")";
+    }
+});
+
+/*
+//create force diagram
+    var force = d3.layout.force()
+        .nodes(d3.values(nodes))
+        .links(links)
+        .size([width, height])
+        .start();
+        //.linkDistance(60)
+        //.charge(-300)
+        //.on("tick", tick);
 
 
+    
+    // Per-type markers, as they don't inherit styles.
+
+    svg.append("defs").selectAll("marker")
+        .data(["suit", "licensing", "resolved"])
+        .enter().append("marker")
+        .attr("id", function(d) { return d; })
+        .attr("viewBox", "0 -5 10 10")
+        .attr("refX", 15)
+        .attr("refY", -1.5)
+        .attr("markerWidth", 6)
+        .attr("markerHeight", 6)
+        .attr("orient", "auto")
+        .append("path")
+        .attr("d", "M0,-5L10,0L0,5");
+
+   var path = svg.append("g").selectAll("path")
+   .data(force.links())
+   .enter().append("path")
+   .attr("class", function(d) { return "link " + d.type; })
+   .attr("marker-end", function(d) { return "url(#" + d.type + ")"; });
+
+var circle = svg.append("g").selectAll("circle")
+   .data(force.nodes())
+   .enter().append("circle")
+   .attr("r", 15)
+   .call(force.drag);
+
+var text = svg.append("g").selectAll("text")
+   .data(force.nodes())
+   .enter().append("text")
+   .attr("x", 8)
+   .attr("y", ".31em")
+   .text(function(d) { return d.name; });
+
+// Use elliptical arc path segments to doubly-encode directionality.
+function tick() {
+ path.attr("d", linkArc);
+ circle.attr("transform", transform);
+ text.attr("transform", transform);
+}
+
+function linkArc(d) {
+ var dx = d.target.x - d.source.x,
+     dy = d.target.y - d.source.y,
+     dr = Math.sqrt(dx * dx + dy * dy);
+ return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
+}
+
+function transform(d) {
+ return "translate(" + d.x + "," + d.y + ")";
+}
+*/
 
 
--- text_preprocessing.py
+++ text_preprocessing.py
@@ -12,7 +12,10 @@ nlp = spacy.load("en_coref_md")
 #print("nlp pipeline components are {}".format(nlp.pipe_names))
 
 #load data
-#input = "I feel that the new visitation policy is discriminatory. I think it should be abolished"
+'''
+input = "I feel that the new visitation policy is discriminatory. I think it should be abolished. I don't like this policy."
+#input = "I feel discriminated by the new visitation policy. I think it should be abolished. I don't like this policy."
+'''
 input = "Robert DeNiro plays the most unbelievably intelligent illiterate of all time. \
 This movie is so wasteful of talent, it is truly disgusting. The script is unbelievable. \
 The dialog is unbelievable. Jane Fonda's character is a caricature of herself, and not a funny one. \
@@ -21,9 +24,9 @@ It also plugs in every cliche in the book. Swoozie Kurtz is excellent in a suppo
 but so what? Equally annoying is this new IMDB rule of requiring ten lines for every review. \
 When a movie is this worthless, it doesn't require ten lines of text to let other readers know \
 that it is a waste of time and tape. Avoid this movie."
+
 doc = nlp(input)
 #print(doc._.coref_resolved)
-#get all coref resolutions and output to json
 
 json_output = defaultdict(list)
 coref_output = defaultdict(list)
@@ -46,14 +49,80 @@ for sent in doc.sents:
     #get sentiment score for each sentence
     #print(sent.text)
     #score and extract sentiment polarity scores
-    #print(sentiAnalyzer.polarity_scores(sent.text))
-    analysis_output = defaultdict(list)
-    analysis_output['sentiment'] = sentiAnalyzer.polarity_scores(sent.text)
-    #print(json.dumps(analysis_output))
-    #implement grammar rules to tease out entity-sentiment relationships
-    for token in sent:
-        #print(token.text, token.pos_, token.dep_)
-        analysis_output['dependency'].append({'text':token.text, 'pos':token.pos_, 'dep':token.dep_})
-    json_output['analysis'].append(analysis_output)
-    #print("\n")
-#print(json.dumps(json_output))
+	#print(sentiAnalyzer.polarity_scores(sent.text))
+	analysis_output = defaultdict(list)
+	analysis_output['sentiment'] = sentiAnalyzer.polarity_scores(sent.text)
+	#print(json.dumps(analysis_output))
+	
+	#implement grammar rules to tease out entity-sentiment relationships
+	for token in sent:
+		#print(token.text, token.pos_, token.dep_)
+		analysis_output['dependency'].append({'text':token.text, 'pos':token.pos_, 'dep':token.dep_})
+    
+	#apply grammar rules here
+	relationship_output = defaultdict(list)
+
+	#initialize
+	subject = ""
+	object = ""
+	temp_object = ""
+
+	for count, token in enumerate(analysis_output['dependency']):
+		#print("token:{}, dep:{}".format(token['text'], token['dep']))
+		
+		### Identifying and extracting subjects ###
+		#Base case: plain old nsubj
+		if token['dep']=='nsubj':
+			subject = token['text']
+			
+			#Rule 1: Check if it is a compound entity
+			if count-1 >= 0:
+				#check not the first token
+				token_minus_1 = analysis_output['dependency'][count-1]
+				if token_minus_1['dep']=='compound' and token['dep']=='nsubj':
+					subject = " ".join([token_minus_1['text'], token['text']])
+			
+			#Rule 2: If nsubj is preceded by possessive and case dependencies, extract compound, poss, case and nsubj
+				#check that there are at least 3 more token behind
+				if count-3 >= 0:
+					token_minus_2 = analysis_output['dependency'][count-2]
+					token_minus_3 = analysis_output['dependency'][count-3]
+					if token_minus_3['dep']=='compound' and token_minus_2['dep']=='poss' and token['dep']=='nsubj':
+						subject = " ".join([token_minus_3['text'], token_minus_2['text']+token_minus_1['text'], token['text']])
+			#print("Subject: {}".format(subject))
+
+		### Identifying and extracting objects ###
+		#Base case: plain old dobj (extracting direct objects)
+		if token['dep']=='dobj':
+			object = token['text']
+			#Rule 1: Check if it is a compound entity
+			if count-1 >= 0:
+				#check not the first token
+				token_minus_1 = analysis_output['dependency'][count-1]
+				if token_minus_1['dep']=='compound' and token['dep']=='dobj':
+					object = " ".join([token_minus_1['text'], token['text']])
+
+		#Rule 2: temporarily store prepositional objects. Direct objects 
+		if token['dep']=='pobj':
+			temp_object = token['text']
+	#Following from rule #2, if no direct object present, use pobj
+	if object == "" and temp_object != "":
+		object = temp_object
+	
+	#Rule 3: Sentences without subjects (Imperative/Axiomatic expression)
+	if object != "" and subject == "":
+		subject = "(Author)"
+	
+	#Rule 4: Sentences without objects ()
+	if object == "" and subject != "":
+		object = subject
+		subject = "(Author)" 
+	
+	#print final object identified
+	#if object != "": print("Object: {}".format(object))
+
+	json_output['raw'].append(analysis_output)
+	json_output['relationship'].append({'subject':subject, 'object':object, 'sentiment':analysis_output['sentiment']['compound']})
+    
+	#print("\n")
+print(json.dumps(json_output))
